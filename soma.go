8c1b01feadc1a010f241b50604500dce0261a696
Francisco Wallison <franciscowallison@gmail.com>
2020-12-20 10:55:24 -0300

    Iniciando teste de Go CI

---
+++ soma.go
@@ -0,0 +1,14 @@
+package main
+import (
+  "fmt"
+  "strconv"
+  "os"
+)
+func main() {
+	valor1,_ := strconv.ParseFloat(os.Args[1],64);
+	valor2,_ := strconv.ParseFloat(os.Args[2],64);
+	fmt.Println(fmt.Sprintf("%f", soma(valor1,valor2)))
+}
+func soma(valor1 float64,valor2 float64) float64{
+	return valor1 + valor2;
+}
